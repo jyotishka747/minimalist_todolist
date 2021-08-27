bc28d29301ad16495d4df680684e811c177e0752
Jyotishka Bhattacharjee <jyotishka2002@gmail.com>
2021-08-27 23:54:26 +0530

    linked js and added DOM, event listener

---
+++ script.js
@@ -0,0 +1,11 @@
+//Selecting the DOM
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+//Event Listeners being added
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
+filterOption.addEventListener("click", filterTodo);
